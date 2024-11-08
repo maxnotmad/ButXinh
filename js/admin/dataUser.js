6a2f58ecff4d90faa2425bc129b43111a8d2e433
maxnotmad <forthesgu2022@gmail.com>
2024-11-08 14:22:30 +0700

    finish-project

---
+++ js/admin/dataUser.js
@@ -0,0 +1,38 @@
+const userArr = [
+    new User(
+        "wizardsc",
+        "wizardsc",
+        "toanhuynh1114@gmail.com",
+        "0814099714",
+        "Huỳnh Phúc Toàn",
+        "118 CN1, P.Sơn Kỳ, Q.Tân Phú, TP.HCM",
+        
+        false,
+        true
+    ),
+    new User(
+        "changsub",
+        "changsub",
+        "leechangsub@gmail.com",
+        // "0814099714",
+        "",
+        "Lee Changsub",
+        "",
+        // "South Korea",
+        
+        false,
+        true
+    ),
+    new User(
+        "duongbinhminh",
+        "duongbinhminh",
+        "duongbinhminh10032004@gmail.com@gmail.com",
+        "0355578938",
+        "Dương Bình Minh",
+        "Sài Gòn, Việt Nam",
+        
+        true,
+        true
+    )
+    
+]
